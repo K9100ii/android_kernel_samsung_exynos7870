3f8523b6fee9a5de76177375ae549ff1a5b3a5e7
Zhao Lei <zhaolei@cn.fujitsu.com>
2015-01-20 15:11:43 +0800

    Btrfs: Include map_type in raid_bio
    Corrent code use many kinds of "clever" way to determine operation
    target's raid type, as:
      raid_map != NULL
      or
      raid_map[MAX_NR] == RAID[56]_Q_STRIPE
    
    To make code easy to maintenance, this patch put raid type into
    bbio, and we can always get raid type from bbio with a "stupid"
    way.
    
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    Signed-off-by: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/volumes.h
+++ fs/btrfs/volumes.h
@@ -304,6 +304,7 @@ typedef void (btrfs_bio_end_io_t) (struct btrfs_bio *bio, int err);
 struct btrfs_bio {
 	atomic_t stripes_pending;
 	struct btrfs_fs_info *fs_info;
+	u64 map_type; /* get from map_lookup->type */
 	bio_end_io_t *end_io;
 	struct bio *orig_bio;
 	unsigned long flags;
