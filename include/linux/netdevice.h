51aa593bc363cbd2841f0270b12d7346aba5437d
Salam Noureddine <noureddine@arista.com>
2015-01-27 11:35:48 -0800

    dev: add per net_device packet type chains
    When many pf_packet listeners are created on a lot of interfaces the
    current implementation using global packet type lists scales poorly.
    This patch adds per net_device packet type lists to fix this problem.
    
    The patch was originally written by Eric Biederman for linux-2.6.29.
    Tested on linux-3.16.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Salam Noureddine <noureddine@arista.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1496,6 +1496,8 @@ struct net_device {
 	struct list_head	napi_list;
 	struct list_head	unreg_list;
 	struct list_head	close_list;
+	struct list_head	ptype_all;
+	struct list_head	ptype_specific;
 
 	struct {
 		struct list_head upper;
