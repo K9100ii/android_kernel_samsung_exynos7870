6138aa30f033b25e5b5fef5595f352bc25b7943a
Trond Myklebust <trond.myklebust@primarydata.com>
2015-02-26 17:36:09 -0500

    NFS: Add attribute update barriers to NFS writebacks
    Ensure that other operations that race with our write RPC calls
    cannot revert the file size updates that were made on the server.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Tested-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -442,6 +442,7 @@ void nfs_mark_request_commit(struct nfs_page *req,
 			     struct pnfs_layout_segment *lseg,
 			     struct nfs_commit_info *cinfo);
 int nfs_write_need_commit(struct nfs_pgio_header *);
+void nfs_writeback_update_inode(struct nfs_pgio_header *hdr);
 int nfs_generic_commit_list(struct inode *inode, struct list_head *head,
 			    int how, struct nfs_commit_info *cinfo);
 void nfs_retry_commit(struct list_head *page_list,
