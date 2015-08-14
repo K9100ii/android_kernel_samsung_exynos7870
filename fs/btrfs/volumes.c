0e5f86db3287c9af340e45b9f5f55b89bd1af859
Anand Jain <anand.jain@oracle.com>
2015-08-14 18:32:48 +0800

    Btrfs: rename btrfs_kobj_add_device to btrfs_sysfs_add_device_link
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -2217,7 +2217,7 @@ int btrfs_init_new_device(struct btrfs_root *root, char *device_path)
 	unlock_chunks(root);
 
 	/* add sysfs device entry */
-	btrfs_kobj_add_device(root->fs_info->fs_devices, device);
+	btrfs_sysfs_add_device_link(root->fs_info->fs_devices, device);
 
 	mutex_unlock(&root->fs_info->fs_devices->device_list_mutex);
 
