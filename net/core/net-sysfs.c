3962c693e38715aeee0e32080c2f29cde5dd30fb
Alexander Stein <alexander.stein@systec-electronic.com>
2015-09-28 15:05:33 +0200

    net sysfs: Print link speed as signed integer
    Otherwise 4294967295 (MBit/s) (-1) will be printed when there is no link.
    Documentation/ABI/testing/sysfs-class-net does not state if this shall be
    signed or unsigned.
    Also remove the now unused variable fmt_udec.
    
    Signed-off-by: Alexander Stein <alexander.stein@systec-electronic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -29,7 +29,6 @@
 static const char fmt_hex[] = "%#x\n";
 static const char fmt_long_hex[] = "%#lx\n";
 static const char fmt_dec[] = "%d\n";
-static const char fmt_udec[] = "%u\n";
 static const char fmt_ulong[] = "%lu\n";
 static const char fmt_u64[] = "%llu\n";
 
@@ -192,7 +191,7 @@ static ssize_t speed_show(struct device *dev,
 	if (netif_running(netdev)) {
 		struct ethtool_cmd cmd;
 		if (!__ethtool_get_settings(netdev, &cmd))
-			ret = sprintf(buf, fmt_udec, ethtool_cmd_speed(&cmd));
+			ret = sprintf(buf, fmt_dec, ethtool_cmd_speed(&cmd));
 	}
 	rtnl_unlock();
 	return ret;
