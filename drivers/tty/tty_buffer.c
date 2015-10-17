f29139640d7694ac3240733ab9eeefee5a18fc90
Peter Hurley <peter@hurleysoftware.com>
2015-10-17 16:36:24 -0400

    tty: Use unbound workqueue for all input workers
    The commonly accepted wisdom that scheduling work on the same cpu
    that handled interrupt i/o benefits from cache-locality is only
    true if the cpu is idle (since bound kworkers are often the highest
    vruntime and thus the lowest priority).
    
    Measurements of scheduling via the unbound queue show lowered
    worst-case latency responses of up to 5x over bound workqueue, without
    increase in average latency or throughput.
    
    pty i/o test measurements show >3x (!) reduced total running time; tests
    previously taking ~8s now complete in <2.5s.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -399,7 +399,7 @@ void tty_schedule_flip(struct tty_port *port)
 	 * flush_to_ldisc() sees buffer data.
 	 */
 	smp_store_release(&buf->tail->commit, buf->tail->used);
-	schedule_work(&buf->work);
+	queue_work(system_unbound_wq, &buf->work);
 }
 EXPORT_SYMBOL(tty_schedule_flip);
 
