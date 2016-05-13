c0bb7ddc548adccf92d62b4293c6171082694949
Kanchanapally, Vidyullatha <vkanchan@qti.qualcomm.com>
2016-05-13 12:50:43 +0530

    qcacld-2.0: Guard driver cfg80211 abort scan implementation with appropriate checks
    prima to qcacld-2.0 propagation
    
    cfg80211 abort scan implementation is available only from
    kernel 4.5 onwards. Hence guard the abort scan driver
    implementation with linux version and backport availability
    checks in order to prevent any compilation issues with old
    kernel versions.
    
    Change-Id: I5956f9daa06225535ae4f10aed4ebe310f7acc7a
    CRs-Fixed: 1010982

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -24124,6 +24124,8 @@ void wlan_hdd_cfg80211_extscan_callback(void *ctx, const tANI_U16 evType,
 
 #endif /* FEATURE_WLAN_EXTSCAN */
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,5,0)) || \
+    defined(CFG80211_ABORT_SCAN)
 /**
  * __wlan_hdd_cfg80211_abort_scan() - cfg80211 abort scan api
  * @wiphy: Pointer to wiphy
@@ -24172,6 +24174,7 @@ static void wlan_hdd_cfg80211_abort_scan(struct wiphy *wiphy,
 	__wlan_hdd_cfg80211_abort_scan(wiphy, wdev);
 	vos_ssr_unprotect(__func__);
 }
+#endif
 
 /* cfg80211_ops */
 static struct cfg80211_ops wlan_hdd_cfg80211_ops =
@@ -24247,5 +24250,8 @@ static struct cfg80211_ops wlan_hdd_cfg80211_ops =
 #ifdef CHANNEL_SWITCH_SUPPORTED
      .channel_switch = wlan_hdd_cfg80211_channel_switch,
 #endif
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,5,0)) || \
+    defined(CFG80211_ABORT_SCAN)
      .abort_scan = wlan_hdd_cfg80211_abort_scan,
+#endif
 };
