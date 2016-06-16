b1c981c0c4cb1719d609f8c0cb33e07a70146cc3
Quentin Armitage <quentin@armitage.org.uk>
2016-06-16 08:00:14 +0100

    ipvs: fix bind to link-local mcast IPv6 address in backup
    commit 3777ed688fba82d0bd43f9fc1ebbc6abe788576d upstream.
    
    When using HEAD from
    https://git.kernel.org/cgit/utils/kernel/ipvsadm/ipvsadm.git/,
    the command:
    ipvsadm --start-daemon backup --mcast-interface eth0.60 \
        --mcast-group ff02::1:81
    fails with the error message:
    Argument list too long
    
    whereas both:
    ipvsadm --start-daemon master --mcast-interface eth0.60 \
        --mcast-group ff02::1:81
    and:
    ipvsadm --start-daemon backup --mcast-interface eth0.60 \
        --mcast-group 224.0.0.81
    are successful.
    
    The error message "Argument list too long" isn't helpful. The error occurs
    because an IPv6 address is given in backup mode.
    
    The error is in make_receive_sock() in net/netfilter/ipvs/ip_vs_sync.c,
    since it fails to set the interface on the address or the socket before
    calling inet6_bind() (via sock->ops->bind), where the test
    'if (!sk->sk_bound_dev_if)' failed.
    
    Setting sock->sk->sk_bound_dev_if on the socket before calling
    inet6_bind() resolves the issue.
    
    Fixes: d33288172e72 ("ipvs: add more mcast parameters for the sync daemon")
    Signed-off-by: Quentin Armitage <quentin@armitage.org.uk>
    Acked-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/netfilter/ipvs/ip_vs_sync.c
+++ net/netfilter/ipvs/ip_vs_sync.c
@@ -1531,7 +1531,8 @@ error:
 /*
  *      Set up receiving multicast socket over UDP
  */
-static struct socket *make_receive_sock(struct netns_ipvs *ipvs, int id)
+static struct socket *make_receive_sock(struct netns_ipvs *ipvs, int id,
+					int ifindex)
 {
 	/* multicast addr */
 	union ipvs_sockaddr mcast_addr;
@@ -1552,6 +1553,7 @@ static struct socket *make_receive_sock(struct netns_ipvs *ipvs, int id)
 		set_sock_size(sock->sk, 0, result);
 
 	get_mcast_sockaddr(&mcast_addr, &salen, &ipvs->bcfg, id);
+	sock->sk->sk_bound_dev_if = ifindex;
 	result = sock->ops->bind(sock, (struct sockaddr *)&mcast_addr, salen);
 	if (result < 0) {
 		pr_err("Error binding to the multicast addr\n");
@@ -1854,7 +1856,7 @@ int start_sync_thread(struct netns_ipvs *ipvs, struct ipvs_sync_daemon_cfg *c,
 		if (state == IP_VS_STATE_MASTER)
 			sock = make_send_sock(ipvs->net, id);
 		else
-			sock = make_receive_sock(ipvs, id);
+			sock = make_receive_sock(ipvs, id, dev->ifindex);
 		if (IS_ERR(sock)) {
 			result = PTR_ERR(sock);
 			goto outtinfo;
