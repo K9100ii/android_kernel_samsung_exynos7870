bf33c362b2c3bf065a357ccbaf91454d35c2aa7e
Mahesh A Saptasagar <c_msapta@qti.qualcomm.com>
2016-07-01 15:41:16 +0530

    qcacld-2.0: Fix for potential buffer overflow and null pointer references
    prima to qcacld-2.0 propagation
    
    Static analyser is reporting errors for array bound
    checking and null pointer references.
    To resolve this
    1. Check for condition array index shouldn't exceed
       WNI_CFG_VALID_CHANNEL_LIST_LEN before accessing
       ChannelList array.
    2. Check for NULL condition wherever necessary.
    
    Change-Id: Idd0a23a8180dddabfdd353c0861899411aecfa16
    CRs-Fixed: 534624

--- drivers/net/wireless/qcacld-2.0/CORE/SME/src/csr/csrNeighborRoam.c
+++ drivers/net/wireless/qcacld-2.0/CORE/SME/src/csr/csrNeighborRoam.c
@@ -4376,6 +4376,9 @@ VOS_STATUS csrNeighborRoamTransitToCFGChanScan(tpAniSirGlobal pMac,
         else
         {
             numOfChannels = pMac->scan.occupiedChannels[sessionId].numChannels;
+            if (numOfChannels > WNI_CFG_VALID_CHANNEL_LIST_LEN) {
+                numOfChannels = WNI_CFG_VALID_CHANNEL_LIST_LEN;
+            }
             if (numOfChannels
 #ifdef FEATURE_WLAN_LFR
                 && ((pNeighborRoamInfo->uScanMode == SPLIT_SCAN_OCCUPIED_LIST) ||
@@ -4407,10 +4410,6 @@ VOS_STATUS csrNeighborRoamTransitToCFGChanScan(tpAniSirGlobal pMac,
                 }
                 else
                 {
-                    if (numOfChannels > WNI_CFG_VALID_CHANNEL_LIST_LEN)
-                    {
-                        numOfChannels = WNI_CFG_VALID_CHANNEL_LIST_LEN;
-                    }
                     vos_mem_copy(channelList,
                             pMac->scan.occupiedChannels[sessionId].channelList,
                             numOfChannels * sizeof(tANI_U8));
@@ -4444,10 +4443,6 @@ VOS_STATUS csrNeighborRoamTransitToCFGChanScan(tpAniSirGlobal pMac,
                 {
                     smsLog(pMac, LOGE, FL("Memory allocation for Channel list failed"));
                     return VOS_STATUS_E_RESOURCES;
-                }
-                if (numOfChannels > WNI_CFG_VALID_CHANNEL_LIST_LEN)
-                {
-                    numOfChannels = WNI_CFG_VALID_CHANNEL_LIST_LEN;
                 }
                 currChannelListInfo->numOfChannels = outputNumOfChannels;
                 vos_mem_copy(currChannelListInfo->ChannelList,
