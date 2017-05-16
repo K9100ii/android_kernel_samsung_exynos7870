86ec04dd9fea02aaec60df36d4cbe03d54989a31
SaidiReddy Yenuga <saidir@codeaurora.org>
2017-05-16 19:28:39 +0530

    qcacld-2.0: Validate NLA attr in hdd_extscan_epno_fill_network_list
    qcacld-3.0 to qcacld-2.0 Propagation.
    
    NLA attribute
    QCA_WLAN_VENDOR_ATTR_PNO_SET_LIST_PARAM_EPNO_NETWORKS_LIST
    used without validation.
    
    validate NLA attribute.
    
    CRs-Fixed: 2048343
    Change-Id: I4da7dbe44325b0ed6346b363b71b558250996f8c

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -4604,6 +4604,10 @@ static int hdd_extscan_epno_fill_network_list(
 	uint8_t index, *ssid;
 
 	index = 0;
+	if (!tb[QCA_WLAN_VENDOR_ATTR_PNO_SET_LIST_PARAM_EPNO_NETWORKS_LIST]) {
+		hddLog(LOGE, FL("attr networks list failed"));
+		return -EINVAL;
+	}
 	nla_for_each_nested(networks,
 			    tb[QCA_WLAN_VENDOR_ATTR_PNO_SET_LIST_PARAM_EPNO_NETWORKS_LIST],
 			    rem1) {
