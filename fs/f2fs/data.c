0279ce1d05ee77744c1cf33ccd70de9e44e7fda5
Chao Yu <yuchao0@huawei.com>
2019-11-01 17:53:23 +0800

    f2fs: show f2fs instance in printk_ratelimited
    [ Upstream commit c45d6002ff7a322022560e9b19ad867b01fec77f ]
    
    As Eric mentioned, bare printk{,_ratelimited} won't show which
    filesystem instance these message is coming from, this patch tries
    to show fs instance with sb->s_id field in all places we missed
    before.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: TALU <taluemail272@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -145,10 +145,11 @@ static bool f2fs_bio_post_read_required(struct bio *bio, int err)
 
 static void f2fs_read_end_io(struct bio *bio, int err)
 {
+	struct f2fs_sb_info *sbi = F2FS_P_SB(bio->bi_io_vec->bv_page);
 	struct page *first_page = bio->bi_io_vec[0].bv_page;
 
-	if (time_to_inject(F2FS_P_SB(bio->bi_io_vec->bv_page), FAULT_READ_IO)) {
-		f2fs_show_injection_info(FAULT_READ_IO);
+	if (time_to_inject(sbi, FAULT_READ_IO)) {
+		f2fs_show_injection_info(sbi, FAULT_READ_IO);
 		err = -EIO;
 	}
 
@@ -177,7 +178,7 @@ static void f2fs_write_end_io(struct bio *bio, int err)
 	int i;
 
 	if (time_to_inject(sbi, FAULT_WRITE_IO)) {
-		f2fs_show_injection_info(FAULT_WRITE_IO);
+		f2fs_show_injection_info(sbi, FAULT_WRITE_IO);
 		err = -EIO;
 	}
 
