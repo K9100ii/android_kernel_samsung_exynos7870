d4a381c258f8226bdd7e3870e12ad93dd6d9dcf9
TALU <taluemail272@gmail.com>
2022-09-02 23:11:04 +0100

    fbdev: exynos: decon_7870: hx8279d: set the brightness value for level 1 in the note table to 1
    Signed-off-by: TALU <taluemail272@gmail.com>

--- drivers/video/fbdev/exynos/decon_7870/panels/hx8279d_param.h
+++ drivers/video/fbdev/exynos/decon_7870/panels/hx8279d_param.h
@@ -925,7 +925,7 @@ static unsigned int brightness_table[EXTEND_BRIGHTNESS + 1] = {
 
 static unsigned int brightness_table_note[EXTEND_BRIGHTNESS + 1] = {
 	0,
-	0, 1, 1, 2, 3, 3, 4, 5, 5, 6, /* 3: 1 */
+	1, 1, 1, 2, 3, 3, 4, 5, 5, 6, /* 3: 1 */
 	7, 7, 8, 9, 9, 10, 11, 11, 12, 13,
 	13, 14, 15, 15, 16, 17, 17, 18, 19, 19,
 	20, 21, 21, 22, 23, 23, 24, 25, 25, 26,
